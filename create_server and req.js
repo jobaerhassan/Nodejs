5421ba649f2d0882f1f6f5192eb556bcbbb5b8e9
jobaerhassan <jobaerhassan3@gmail.com>
2021-07-30 15:53:41 +0600

    server create

--- create_server and req.js
+++ create_server and req.js
@@ -1,8 +1,8 @@
 const http = require('http');
 
 const server = http.createServer((req , res)=> {
-    console.log(req);
-    process.exit();
+    console.log(req.url , req.headers , req.method);
+    //process.exit();
 })
 server.listen(5000);
 //(another method) ->  function rqListener(req , res) {
