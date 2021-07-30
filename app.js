f6982f9cef256328e608c4a1e09198c1a0e67a87
jobaerhassan <jobaerhassan3@gmail.com>
2021-07-30 12:33:42 +0600

    server

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log('bal')
+const http = require('http');
+
+const server = http.createServer((req , res)=> {
+    console.log(req);
+    process.exit();
+})
+server.listen(5000);
+//(another method) ->  function rqListener(req , res) {
+
+// } 
+
+// http.createServer(rqListener);
+
+// (another method) -> http.createServer(function(req , res){
+
+// });
