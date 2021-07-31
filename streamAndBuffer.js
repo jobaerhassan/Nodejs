1be893568f030cb78f53b15a32809b0e5c7d9d9e
jobaerhassan <jobaerhassan3@gmail.com>
2021-07-31 22:04:36 +0600

    read data with stream and buffer

--- streamAndBuffer.js
+++ streamAndBuffer.js
@@ -1,1 +1,8 @@
 const fs = require('fs')
+
+const ourReadStream = fs.createReadStream(`${__dirname}/bigText.txt`);
+
+ourReadStream.on('data', (data) => {
+    console.log(data);
+})
+console.log(`hello`)
