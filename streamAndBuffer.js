88e2e2ee05a5a1a713e5fb9b6fa3809ae50cad6e
jobaerhassan <jobaerhassan3@gmail.com>
2021-08-01 08:26:33 +0600

    pipe

--- streamAndBuffer.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs')
-
-const ourReadStream = fs.createReadStream(`${__dirname}/bigText.txt`);
-
-ourReadStream.on('data', (data) => {
-    console.log(data);
-})
-console.log(`hello`)
